dc5de921b25dd95a11e0164bbb1685dbc51ef548
Nooya99 <shaygarz001@gmail.com>
2026-07-31 19:04:10 +0700

    chore: remove unused static-site and scratch files to optimize project

--- test-db.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const products = await prisma.product.findMany();
-  console.log(products.map(p => ({ id: p.id, discount: p.discount, discountExpiresAt: p.discountExpiresAt })));
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
